46ccdb75110c9f1dda61a595f4a90ee084d12d38
hirwajeaneric <hirwajeaneric25@gmail.com>
2022-08-11 16:19:29 +0200

    Project done

--- src/components/Contacts/AddContact/AddContact.jsx
+++ src/components/Contacts/AddContact/AddContact.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState , useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ContactService } from '../../../services/ContactService';
 
 const AddContact = () => {
+
+  let navigate = useNavigate();
+
+  let [state, setState] = useState({
+    loading: false,
+    contact: {
+      name: "",
+      photo: "",
+      mobile: "",
+      email: "",
+      company: "",
+      title: "",
+      groupId: ""
+    },
+    groups: [],
+    errorMessage: ''
+  });
+
+  let updateInput = (event)=> {
+    setState({
+      ...state, 
+      contact: {
+        ...state.contact,
+        [event.target.name] : event.target.value
+      }
+    });
+  };
+  
+  useEffect(async ()=>{
+    try {
+      setState({...state, loading: true});
+      let response = await ContactService.getGroups();
+      setState({
+        ...state,
+        loading: false,
+        groups: response.data
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        errorMessage: error.message
+      });
+    }
+  },[]);
+
+  let submitForm = async (event) => {
+    event.preventDefault();
+    try {
+      let response = await ContactService.createContact(state.contact);
+      if(response){
+        navigate('/contacts/list', {replace: true});
+      }
+    } catch (error) {
+      setState({...state.contact, errorMessage: error.message});
+      navigate('/contacts/add', {replace: false});
+    }
+  };
+
+  let {loeading, contact, groups, errorMessage} = state;
+
   return (
     <React.Fragment>
         <section className='add-contact p-3'>
@@ -17,28 +78,36 @@ const AddContact = () => {
               </div>
               <div className='row'>
                 <div className='col-md-4'>
-                  <form>
+                  <form onSubmit={submitForm}>
                     <div className="mb-2">
-                      <input type='text' className='form-control' placeholder='Name' />
+                      <input type='text' className='form-control' placeholder='Name' required="true" name="name" value={contact.name} onChange={updateInput}/>
                     </div>
                     <div className="mb-2">
-                      <input type='text' className='form-control' placeholder='Photo Url' />
+                      <input type='text' className='form-control' placeholder='Photo Url' required="true" name="photo" value={contact.photo} onChange={updateInput}/>
                     </div>
                     <div className="mb-2">
-                      <input type='text' className='form-control' placeholder='Mobile' />
+                      <input type='number' className='form-control' placeholder='Mobile' required="true" name="mobile" value={contact.mobile} onChange={updateInput}/>
                     </div>
                     <div className="mb-2">
-                      <input type='text' className='form-control' placeholder='Email' />
+                      <input type='text' className='form-control' placeholder='Email' required="true" name="email" value={contact.email} onChange={updateInput}/>
                     </div>
                     <div className="mb-2">
-                      <input type='text' className='form-control' placeholder='Company' />
+                      <input type='text' className='form-control' placeholder='Company' required name="company" value={contact.company} onChange={updateInput}/>
                     </div>
                     <div className="mb-2">
-                      <input type='text' className='form-control' placeholder='Title' />
+                      <input type='text' className='form-control' placeholder='Title' required="true" name="title" value={contact.title} onChange={updateInput}/>
                     </div>
                     <div className="mb-2">
-                      <select className='form-control'>
+                      <select className='form-control' required="true" name='groupId' value={contact.groupId} onChange={updateInput}>
                         <option value=''>Select a Group</option>
+                        {
+                            groups.length > 0 &&
+                                groups.map(group => {
+                                    return(
+                                        <option key={group.id} value={group.id}>{group.name}</option>
+                                    )
+                                })
+                        }
                       </select>
                     </div>
                     <div className="mb-2">
--- src/components/Contacts/ContactList/ContactList.jsx
+++ src/components/Contacts/ContactList/ContactList.jsx
@@ -6,9 +6,14 @@ import Spinner from '../../Spinner/Spinner';
 
 const ContactList = () => {
 
+  let [query, setQuery] = useState({
+    text: ''
+  });
+
   let [state, setState] = useState({
     loading: false,
     contacts: [],
+    filteredContacts: [],
     errorMessage: ''
   });
 
@@ -20,7 +25,8 @@ const ContactList = () => {
         setState({
           ...state, 
           loading: false,
-          contacts: fetchedContacts
+          contacts: fetchedContacts,
+          filteredContacts: fetchedContacts
         });
       })
       .catch((error) => {
@@ -32,7 +38,42 @@ const ContactList = () => {
       })
   },[]);
 
-  let {loading, contacts, errorMessage} = state;
+  // Delete contact
+  let clickDelete = async (contactId) => {
+    try {
+      let response = await ContactService.deleteContact(contactId);
+      if (response) {
+        setState({...state, loading: true});
+        let response = await ContactService.getAllContacts();
+        setState({
+          ...state,
+          loading: false,
+          contacts: response.data,
+          filteredContacts: response.data
+        });
+      }
+    } catch (error) {
+        setState({
+          ...state, 
+          loading: false,
+          errorMessage: error.message
+        })
+    }
+  };
+
+  let searchContacts = (event) => {
+    setQuery({...query, text: event.target.value});
+    let theContacts = state.contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(event.target.value.toLowerCase())
+    });
+    setState({
+      ...state, 
+      filteredContacts: theContacts
+    })
+
+  }
+
+  let {loading, contacts, filteredContacts, errorMessage} = state;
 
   return (
     <React.Fragment>
@@ -59,12 +100,13 @@ const ContactList = () => {
                   <form className='row'>
                     <div className='col'>
                       <div className='mb-2'>
-                        <input type='text' className='form-control' placeholder='Search Names' />
-                      </div>
-                    </div>
-                    <div className='col'>
-                      <div className='mb-2'>
-                        <input type='submit' className='btn btn-outline-dark' value='Search' />
+                        <input
+                          name='text'
+                          value={query.text}
+                          onChange={searchContacts} 
+                          type='text' 
+                          className='form-control' 
+                          placeholder='Search Names' />
                       </div>
                     </div>
                   </form>
@@ -80,7 +122,8 @@ const ContactList = () => {
               <div className='container'>
                 <div className='row'>
                   {
-                    contacts.length > 0 && contacts.map(contact => {
+                    contacts.length > 0 &&
+                    filteredContacts.map(contact => {
                       return (
                         <div className='col-md-6' key={contact.id}>
                           <div className='card my-2'>
@@ -109,7 +152,7 @@ const ContactList = () => {
                                   <Link to={`/contacts/edit/${contact.id}`} className='btn btn-primary my-1'>
                                     <i className='fa fa-pen'/>
                                   </Link>
-                                  <button className='btn btn-danger my-1'>
+                                  <button className='btn btn-danger my-1' onClick={()=> clickDelete(contact.id)}>
                                     <i className='fa fa-trash'/>
                                   </button>
                                 </div>
